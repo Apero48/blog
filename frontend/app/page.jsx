e96a7fc0157c091c0cc5a71f31a4a4a7fd2110ec
Apero48 <raoufni48@gmail.com>
2025-08-04 10:52:25 -0400

    Create page.jsx

---
+++ frontend/app/page.jsx
@@ -0,0 +1,10 @@
+// app/page.jsx
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Bienvenue sur mon blog !</h1>
+      <p>Ceci est la page d’accueil. Profitez de votre visite.</p>
+    </main>
+  );
+}
